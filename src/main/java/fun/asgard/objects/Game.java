d0118f8cdbfc158f1ff7962c5d77ea7b7ee21a17
Andrew <65760867+VolanDeMor1@users.noreply.github.com>
2021-06-02 21:20:28 +0300

    Fixed a bunch of bugs

--- src/main/java/fun/asgard/objects/Game.java
+++ src/main/java/fun/asgard/objects/Game.java
@@ -2,19 +2,26 @@ package fun.asgard.objects;
 
 import fun.asgard.events.GameStartEvent;
 import fun.asgard.events.GameStopEvent;
+import fun.asgard.events.PlayerConnectEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.HashSet;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Game {
 
     private final World world;
     private final String gameName;
+    private final Plugin plugin;
     private long time;
     private final HashSet<Player> players = new HashSet<>();
+    private boolean kickOnLeave = false;
 
     /**
      *
@@ -22,7 +29,8 @@ public class Game {
      * @param gameName Name of the game
      * @param time Game time
      */
-    public Game(World world, String gameName, long time) {
+    public Game(Plugin plugin, World world, String gameName, long time) {
+        this.plugin = plugin;
         this.gameName = gameName;
         this.world = world;
         this.time = time;
@@ -30,18 +38,16 @@ public class Game {
 
     /**
      * After calling the method, GameStartEvent will be triggered
-     * @return Game
      */
-    public Game start() {
+    public void start() {
         Bukkit.getPluginManager().callEvent(new GameStartEvent(this, this.players.toArray(new Player[0])));
-        return this;
     }
 
     /**
      * After calling the method, GameEndEvent will be triggered
      */
     public void stop(boolean saveWorld) {
-        new GameStopEvent(this, (Player[]) this.players.toArray());
+        this.plugin.getServer().getPluginManager().callEvent(new GameStopEvent(this, (Player[]) this.players.toArray()));
         this.shutdown(saveWorld);
     }
 
@@ -49,7 +55,7 @@ public class Game {
      * After calling the method, GameEndEvent will be triggered
      */
     public void stop(Player winner, Player loser, boolean saveWorld) {
-        new GameStopEvent(this, (Player[]) this.players.toArray(), winner, loser);
+        this.plugin.getServer().getPluginManager().callEvent(new GameStopEvent(this, (Player[]) this.players.toArray(), winner, loser));
         this.shutdown(saveWorld);
     }
 
@@ -57,7 +63,7 @@ public class Game {
      * After calling the method, GameEndEvent will be triggered
      */
     public void stop(Player[] winners, Player[] losers, boolean saveWorld) {
-        new GameStopEvent(this, (Player[]) this.players.toArray(), winners, losers);
+        this.plugin.getServer().getPluginManager().callEvent(new GameStopEvent(this, (Player[]) this.players.toArray(), winners, losers));
         this.shutdown(saveWorld);
     }
 
@@ -65,7 +71,7 @@ public class Game {
      * After calling the method, GameEndEvent will be triggered
      */
     public void stop(HashSet<Player> winners, HashSet<Player> losers, boolean saveWorld) {
-        new GameStopEvent(this, this.players, winners, losers);
+        this.plugin.getServer().getPluginManager().callEvent(new GameStopEvent(this, this.players, winners, losers));
         this.shutdown(saveWorld);
     }
 
@@ -78,12 +84,18 @@ public class Game {
         this.players.clear();
     }
 
-    public void removePlayer(Player player) {
-        this.players.remove(player);
+    public void runGameTask(@NotNull Runnable task, long delay, long period) {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(task, delay, period, TimeUnit.MILLISECONDS);
     }
 
-    public void addPlayer(Player player) {
+    public void connectPlayer(Player player) {
         this.players.add(player);
+        this.plugin.getServer().getPluginManager().callEvent(new PlayerConnectEvent(this, player));
+    }
+
+    public void disconnectPlayer(Player player) {
+        this.players.remove(player);
     }
 
     public HashSet<Player> getPlayers() {
@@ -104,5 +116,23 @@ public class Game {
 
     public void setTime(long time) {
         this.time = time;
+    }
+
+    public void setKickOnLeave(boolean kickOnLeave) {
+        this.kickOnLeave = kickOnLeave;
+    }
+
+    public boolean isKickOnLeave() {
+        return kickOnLeave;
+    }
+
+    @Override
+    public String toString() {
+        return "Game { " +
+                "world = " + world.getName() +
+                ", gameName = '" + gameName + '\'' +
+                ", time = " + time +
+                ", players = " + players +
+                '}';
     }
 }
