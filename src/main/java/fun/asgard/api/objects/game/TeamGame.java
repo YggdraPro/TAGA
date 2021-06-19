a963364cba17c2e0e6dede86bc8172f2f0799d8c
Andrew <65760867+VolanDeMor1@users.noreply.github.com>
2021-06-19 12:42:14 +0300

    Delete src directory

--- src/main/java/fun/asgard/api/objects/game/TeamGame.java
+++
@@ -1,9 +0,0 @@
-package fun.asgard.api.objects.game;
-
-import fun.asgard.api.objects.managers.PlayersManager;
-
-public interface TeamGame extends BaseGame {
-
-    PlayersManager getPlayersManager();
-
-}
